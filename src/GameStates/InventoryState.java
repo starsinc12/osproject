f2f99c00ebc34953b900f75dcbb5df80911fd989
cordanius240 <vantedf@mail.ru>
2021-04-19 21:15:30 -0700

    pachahi cartinki

--- src/GameStates/InventoryState.java
+++ src/GameStates/InventoryState.java
@@ -1,13 +1,26 @@
 package GameStates;
 
+import logic.GameLogic;
 import managers.GameStateManager;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class InventoryState extends GameState {
     public InventoryState(GameStateManager gameStateManager) {
         super(gameStateManager);
     }
+Image img;
+    {
+        try{
+            img= ImageIO.read(new File("src\\images\\backs\\inventory.png"));
+
+        } catch (
+    IOException e) {
+        e.printStackTrace();
+    } }
 
     @Override
     public void init() {
@@ -21,6 +34,6 @@ public class InventoryState extends GameState {
 
     @Override
     public void draw(Graphics2D g) {
-
+        g.drawImage(img,0,0, GameLogic.WIDTH,GameLogic.HEIGHT,null);
     }
 }
